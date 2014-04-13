bdfc8930cc1484459db21f083137316c55bdee7b
KUCKLU <kucklu@visualive.jp>
2014-04-13 12:16:26 +0900

    説明修正

--- simple-expires.php
+++ simple-expires.php
@@ -1,33 +1,28 @@
 <?php
 /*
 Plugin Name: Simple expires
-Plugin URI: http://www.wordpress.org
-Description: Add expire date for post and page.
-Author: Andrea Bersi
-Version: 0.10
-Author URI: http://www.andreabersi.com/
-*/
+Plugin URI: https://github.com/VisuAlive/simple-expires
+Description: He is an inheritor of Simple Expires by which Mr. abmcr was created.
+Author: VisuAlive
+Version: 1.0.0
+Author URI: http://visualive.jp/
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-/*
-Copyright (c) 2010 Andrea Bersi.
+VisuAlive WordPress Plugin, Copyright (C) 2014 VisuAlive Inc
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 define('SIMPLE_EXPIRES_PLUGIN_URL', WP_PLUGIN_URL.'/'.dirname(plugin_basename(__FILE__)));
